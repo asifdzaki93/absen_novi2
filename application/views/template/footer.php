1bdae0ba518999e2f6ad7a8beca65b0e0d036034
asifdzaki93 <asifdzaki93@gmail.com>
2024-06-29 20:10:33 +0700

    db

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.3.1
     </div>
-    <strong> &copy; 2022 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE. | Repost by <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'>ShareCodingan.</a>
-	
+    <strong> &copy; 2024 <a href="#">Absensi Karyawan QR Code</a>.</strong> by <a href='https://nvm.my.id/' title='nvm.my.id' target='_blank'>Novi Masrokhatul Fu'adah.</a>
+
 </footer>
